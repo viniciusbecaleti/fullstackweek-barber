c77c44a8f10c5741cca86e4771039b3cad01a3c7
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-01-30 22:57:27 -0300

    feat: add initial booking item component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { ptBR } from 'date-fns/locale'
 
 import { Header } from "@/components/header"
 import { Search } from './components/search'
+import { BookingItem } from '@/components/booking-item'
 
 export default function Home() {
   const date = new Date()
@@ -34,6 +35,11 @@ export default function Home() {
 
       <div className='px-5 mt-6'>
         <Search />
+      </div>
+
+      <div className='px-5 mt-9'>
+        <h2 className='mb-3 uppercase text-xs text-gray-400 font-bold'>Agendamentos</h2>
+        <BookingItem />
       </div>
     </>
   )
