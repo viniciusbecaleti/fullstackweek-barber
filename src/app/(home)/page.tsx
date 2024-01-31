8f7898b9bf95a915c0f388f051d1bd112248eaf1
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-01-30 21:45:30 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,28 @@
-import { Header } from "@/components/header";
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+import { Header } from "@/components/header"
 
 export default function Home() {
+  const date = new Date()
+
   return (
     <>
       <Header />
+
+      <div className='px-5 pt-5'>
+        <h2 className='text-xl font-bold'>Olá, Miguel</h2>
+        <p className='text-sm'>
+          <span className='capitalize'>
+            {format(date, "EEEE", {
+              locale: ptBR
+            })}
+          </span>
+          {format(date, "',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
     </>
   )
 }
